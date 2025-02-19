012aea981aa38d006da03d26c0b2a96fdc13c46a
CarlosEduardoNoriega <L24170654@culiacan.tecnm.mx>
2025-02-19 15:58:11 +0000

    Actualizacion de getGreeting

--- biblioteca/Invitado.java
+++ biblioteca/Invitado.java
@@ -4,8 +4,12 @@ public class Invitado extends Persona {
     private String institucion;
 
     public Invitado(){}
+
     public Invitado(int id_Persona, String nombre, String telefono, String institucion){
         super( id_Persona , nombre ,  telefono);
         this.institucion = institucion;
     }
+
+    public void consultarPrestamos(){}
+    public void 
 }
